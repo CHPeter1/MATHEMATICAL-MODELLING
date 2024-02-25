447015165594bcf3c02bc335c74e6b82fad509c5
Peter Ngaira <peterngaira2@gmail.com>
2024-02-25 14:08:41 +0300

    BRIEF NOTES OF EPIDEMIC MODELLING

---
+++ MODELING AN EPIDEMIC.R
@@ -0,0 +1,29 @@
+##MODELLING AN EPIDEMIC
+##Why do we model epidemics?
+#Predict the current conditions
+#Predict future progress of any given epidemic
+#Understand the mechanisms behind the epidemic
+#Understand which interventions might be successful
+
+##How do we model epidemics?
+#Identify key biological processes
+#Abstract them into a mathematical model
+#Investigate effect(s) of changing parameters
+
+## SI MODEL
+#Assume
+#fixed population of constant size (S+I=N)
+#All members of the population are in contact
+
+#Rate at which a susceptible person gets infected depends upon:
+#The number or proportion of infected persons
+#The transmission rate β
+
+##Rate of infection
+#We are going to focus on frequency dependent transmission
+#This is the most appropriate for models in humans
+#Livestock models may use density dependent transmission
+
+
+
+
